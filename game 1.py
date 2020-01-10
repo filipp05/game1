e28151b50fb02904f618bcfa8be6721ab2b16873
Timofey <tmstarodubtsev@edu.hse.ru>
2020-01-10 19:10:19 +0300

    first steps...

--- game 1.py
+++ game 1.py
@@ -3,3 +3,35 @@ class World:
         self.max_err = max_err
         self.current_day = 1
         self.days = []
+
+    def show_wellcome_message(self):
+        # todo
+        print("ткст приветствия из предыдущей игры")
+
+
+class DayTask:
+    def __init__(self, day_quest, day_corr_ans, day_variants):
+        self.question = day_quest
+        self.correct_ans = day_corr_ans
+        self.ans_variants = day_variants
+
+    def show(self):
+        print(self.question)
+        print(self.ans_variants)
+
+    def is_day_complete(self, user_ans):
+        return user_ans == self.correct_ans
+
+
+class User:
+    def __init__(self):
+        self.errors = 0
+        self.level = 0
+
+
+def main():
+    max_err = int(input())
+    world = World(max_err)
+    world.show_wellcome_message()
+
+main()
